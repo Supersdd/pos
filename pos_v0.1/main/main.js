b6968bbbfa3847b35d7c9891f2daf84eb235ec67
Supersdd <sdd942155@outlook.com>
2015-07-24 15:20:17 +0800

    pos_v0.1,pos_v0.2 finished

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,29 +1,78 @@
+function get_same_elements(collection_a,collection_b) {
+  collection_b.forEach(function(val) {
+    if(val.name === collection_a.name) {
+      collection_a["barcode"] = val.barcode;
+      collection_a["unit"] = val.unit;
+      collection_a["price"] = val.price;
+    }
+  })
+  return collection_a;
+}
+
 function printReceipt(inputs) {
   var result = '***<没钱赚商店>收据***\n';
   var temp = {};
   var temp_inputs = [];
-  var sum = 0;
   inputs.forEach(function (val) {
     temp[val.name] = temp[val.name] ? temp[val.name] + 1 : 1;
   })
-  inputs.forEach(function (val) {
-    if (temp_inputs.indexOf(val) ==! -1) {
-      temp_inputs.push(val);
-    }
+  for (var i in temp) {
+    temp_inputs.push({name: i, count: temp[i]});
+  }
+  temp_inputs.forEach(function(item) {
+
+    get_same_elements(item,inputs);
+
+    result += '名称：' + val.name + '，' +
+      '数量：' + val.count + val.unit + '，' +
+      '单价：' + val.price + '.00' + '(元)' + '，' +
+      '小计：' + val.count * val.price + '.00' + '(元)' + '\n';
   })
-  result += print(temp_inputs,temp);
+
+  var sum = get_total_price(temp_inputs);
+
+  result += '----------------------\n' +
+    '总计：' + sum + '.00' + '(元)' + '\n' +
+    '**********************';
   console.log(result);
 }
 
-var print = function(collection,object,result) {
-  for (var item in object) {
-    if(collection.name === item) {
-      var temp1   = '名称：' + collection.name + '，' +
-        '数量：' + object[item] + collection.unit + '，' +
-        '单价：' + collection.price + '.00' + '(元)' + '，' +
-        '小计：' + object[item] * collection.price + '.00' + '(元)' + '\n';
-    }
-  }
-  return temp1;
+var get_total_price = function(item) {
+  var total_price =0;
+  item.forEach(function(val) {
+    total_price += val.price * val.count;
+  })
+  return total_price
 }
 
+
+
+
+
+
+
+
+
+
+
+
+//    }
+//  })
+//  temp_inputs.forEach(function(val2) {
+//    result += print(val2,temp);
+//  })
+//  console.log(result);
+//}
+//
+//var print = function(collection,object) {
+//  for (var item in object) {
+//    if(collection.name === item) {
+//      var temp1 = '名称：' + collection.name + '，' +
+//                  '数量：' + object[item] + collection.unit + '，' +
+//                  '单价：' + collection.price + '.00' + '(元)' + '，' +
+//                  '小计：' + object[item] * collection.price + '.00' + '(元)' + '\n';
+//    }
+//  }
+//  return temp1;
+//}
+//
--- pos_v0.2/main/main.js
+++ pos_v0.2/main/main.js
@@ -1,4 +1,48 @@
-//TODO: Please write code in this file.
-function printReceipt(inputs) {
+function get_same_item(collection_a,collection_b) {
+  collection_b.forEach(function(val) {
+    if(collection_a.barcode ===  val.barcode) {
+      collection_a["name"] = val.name;
+      collection_a["unit"] = val.unit;
+      collection_a["price"] = val.price;
+    }
+  })
+  return collection_a
+}
 
+function get_total_price(thing) {
+  var total_price = 0;
+  thing.forEach(function(val) {
+    total_price += val.price * val.count;
+  })
+  return total_price;
+}
+
+
+function printReceipt(inputs) {
+  var allItems = loadAllItems();
+  var result = '***<没钱赚商店>收据***\n';
+  var temp = {};
+  var collected_inputs = [];
+  inputs.forEach(function(item) {
+    temp[item] = temp[item] ? temp[item] + 1 : 1;
+  })
+  for (var i in temp) {
+    collected_inputs.push({barcode: i, count: temp[i]});
+  }
+  collected_inputs.forEach(function(val) {
+
+    get_same_item(val,allItems);
+
+    result += '名称：' + val.name + '，' +
+      '数量：' + val.count + val.unit + '，' +
+      '单价：' + val.price + '.00' + '(元)' + '，' +
+      '小计：' + val.count * val.price + '.00' + '(元)' + '\n';
+  })
+
+  var sum = get_total_price(collected_inputs);
+
+  result += '----------------------\n' +
+    '总计：' + sum + '.00' + '(元)' + '\n' +
+    '**********************';
+  console.log(result);
 }
